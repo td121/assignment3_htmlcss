a2814fd857e85c0a3e659cb55c4da3676084a7b6
td121 <tong.ding2020@gmail.com>
2022-03-12 16:21:27 +0800

    test

--- SpeakGoodBye.js
+++
@@ -1,8 +0,0 @@
-(function(window){
-  var speakWord = "Good Bye";
-  var saybye = function (name){
-    console.log(speakWord + ' ' + name)
-  }
-  window.saybye = saybye
-})(window);
-
--- SpeakHello.js
+++
@@ -1,9 +0,0 @@
-(function(window){
-  var speakWord = 'Hello';
-  var sayhello = function (name){
-    console.log(speakWord + ' ' + name)
-  }
-  window.sayhello = sayhello
-})(window);
-
-
